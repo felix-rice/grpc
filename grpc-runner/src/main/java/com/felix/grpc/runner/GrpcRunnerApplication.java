facb5e2d53ad3d0a3d4600a8363eb42a1adc7c33
lixin40 <lixin40@kuaishou.com>
2022-06-14 18:04:00 +0800

    init repository

---
+++ grpc-runner/src/main/java/com/felix/grpc/runner/GrpcRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.felix.grpc.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcRunnerApplication.class, args);
+    }
+
+}
